0d0acf7183d183a1b5ac3b3d25c10bd1560e1744
syedatif <syed.atifgd7@gmail.com>
2018-12-03 21:13:03 +0500

    learn swift initial

---
+++ The Knight Plan/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  The Knight Plan
+//
+//  Created by MacBook Pro on 03/12/2018.
+//  Copyright © 2018 Code Avenue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ The Knight PlanTests/The_Knight_PlanTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Knight_PlanTests.swift
+//  The Knight PlanTests
+//
+//  Created by MacBook Pro on 03/12/2018.
+//  Copyright © 2018 Code Avenue. All rights reserved.
+//
+
+import XCTest
+@testable import The_Knight_Plan
+
+class The_Knight_PlanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
